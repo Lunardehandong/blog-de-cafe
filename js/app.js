71b2488ff95afaeedb58aa590843b2e61ae6579c
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-07-10 15:58:20 -0600

    Update heading text and log to console

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+// querySelector
 
+// Selecciona el primer elemento h2 que esté dentro de una clase llamada .header__texto
+const heading = document.querySelector('.header__texto h2'); // Retorna 0 o 1 Elementos
+
+// Modifica el contenido de texto del elemento seleccionado
+heading.textContent = 'Nuevo Texto'; 
+
+// Muestra el elemento HTML modificado en la consola
+console.log(heading);
+
+
+// // querySelectorAll
+
+
+// // getElementById
